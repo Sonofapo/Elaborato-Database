138929cf83bcd623367e53d7951899c6ebab3c55
Sonofapo <github@sonofapo.slmail.me>
2023-07-23 13:14:11 +0200

    Implementazione logica simulazione

--- src/class/DB.class.php
+++ src/class/DB.class.php
@@ -41,11 +41,15 @@ class DB {
 	}
 
 	public function getRandomMap() {
-		return $this->query("SELECT Nome from mappe ORDER BY RAND() LIMIT 1");
+		return $this->query("SELECT Nome, NumeroSiti from mappe ORDER BY RAND() LIMIT 1");
 	}
 
 	public function getRandomAgents() {
 		return $this->query("SELECT Nome from agenti ORDER BY RAND() LIMIT 5");
+	}
+
+	public function getRandomWeapon($tipo) {
+		return $this->query("SELECT Nome, Tipologia from armi PRDER BY RAND() LIMIT 1");
 	}
 
 	public function createMatch($mappa) {
@@ -60,6 +64,28 @@ class DB {
 		# CodiceGiocatore auto-increment
 		$this->query("INSERT INTO giocatori (UsernameUtente, CodicePartita, NomeAgente, NomeSquadra)
 			VALUES (?, ?, ?, ?)", [$username, $agente, $partita, $squadra], "ssis");
+		return $this->query("SELECT Codice from giocatori ORDER BY Codice DESC LIMIT 1")[0]["Codice"];
+	}
+
+	public function saveRound($codice, $numero, $durata, $squadra, $ruolo) {
+		$this->query("INSERT INTO (round CodicePartita, Numero, Durata, SquadraVincente, RuoloVincente)
+			VALUES (?, ?, ?, ?, ?)", [$codice, $numero, $durata, $squadra, $ruolo], "iisss");
+	}
+
+	public function saveAction($codice, $round, $tipo, $giocatore, $sito) {
+		$this->query("INSERT INTO azioni (CodicePartitaRound, NumeroRound, Tipo, CodiceGiocatore, Sito)
+			VALUES (?, ?, ?, ?, ?)", [$codice, $round, $tipo, $giocatore, $sito], "iisis");
+	}
+
+	public function savePurchase($codice, $round, $arma, $giocatore) {
+		$this->query("INSERT INTO possessi (CodicePartitaRound, NumeroRound, NomeArma, CodiceGiocatore)
+			VALUES (?, ?, ?, ?)", [$codice, $round, $arma, $giocatore], "iiss");
+	}
+
+	public function saveKill($codice, $round, $giocatoreS, $giocatoreC, $tempo, $arma) {
+		$this->query("INSERT INTO uccisioni (CodicePartitaRound, NumeroRound, CodiceGiocatoreS,
+			CodiceGiocatoreC, Istante, NomeArma) VALUES (?, ?, ?, ?, ?, ?)",
+			[$codice, $round, $giocatoreS, $giocatoreC, $tempo, $arma], "iissss");
 	}
 
 	/** STATISTIC FUNCTIONS */
--- src/class/Game.class.php
+++ src/class/Game.class.php
@@ -2,41 +2,115 @@
 
 class Game {
 
+	const A_TEAM_KILL = 0;
+	const D_TEAM_KILL = 1;
+
 	private $database;
 	private $Codice;
-	private $TeamA;
-	private $TeamB;
+	private $sites;
+	private $Alpha;
+	private $Beta;
+	private $round;
+	private $role; # always refer to Alpha
 
 	function __construct($database) {
 		$this->database = $database;
-		$this->Codice = $this->database->createMatch($this->database->getRandomMap()[0]["Nome"]);
+		$map = $this->database->getRandomMap()[0];
+		# convert N to first N letter of alphabet array (caps)
+		$this->sites = @array_splice(range('A', 'C'), 0, $map["NumeroSiti"]);
+		$this->Codice = $this->database->createMatch($map["Nome"]);
 		$this->composeTeams();
+		$this->round = 1;
+		$this->role = ["Attacco", "Difesa"][rand() % 2];
 	}
 
 	public function composeTeams() {
 		# select 10 random users
 		$users = $this->database->getRandomUsers();
 		# select 5 random agents for each team
-		$this->TeamA = $this->database->getRandomAgents();
-		$this->TeamB = $this->database->getRandomAgents();
+		$this->Alpha = $this->database->getRandomAgents();
+		$this->Beta = $this->database->getRandomAgents();
 
-		# create a player with username-agent pair. [0-4]: teamA, [5-9]: teamB
-		for ($i = 0; $i < count($this->TeamA); $i++)
-			$this->TeamA[$i] = $this->generatePlayer($users[$i], $this->TeamA[$i], "Alpha");
-		for ($i = 0; $i < count($this->TeamB); $i++)
-			$this->TeamB[$i] = $this->generatePlayer($users[$i+5], $this->TeamB[$i], "Beta");
+		# create a player with username-agent pair. [0-4]: Alpha, [5-9]: Beta
+		for ($i = 0; $i < count($this->Alpha); $i++)
+			$this->Alpha[$i] = $this->generatePlayer($users[$i], $this->Alpha[$i], "Alpha");
+		for ($i = 0; $i < count($this->Beta); $i++)
+			$this->Beta[$i] = $this->generatePlayer($users[$i+5], $this->Beta[$i], "Beta");
 	}
 
 	public function generatePlayer($username, $agente, $squadra) {
-		$player = [
-			"Codice" => 0,
-			"UsernameUtente" => $username,
-			"CodicePartita" => $this->Codice,
-			"NomeAgente" => $agente,
-			"NomeSquadra" => $squadra,
-		];
-		$this->database->createPlayer($username, $agente, $this->Codice, $squadra);
+		$codice = $this->database->createPlayer($username, $agente, $this->Codice, $squadra);
+		$player = [ "c" => $codice, "a" => [] ];
 		return $player;
+	}
+
+	public function generateRound() {
+		# random purchases for each team member
+		foreach (["Alpha", "Beta"] as $team) {
+			for ($i = 0; $i < count($this->$team); $i++) {
+				if (rand() % 100 < 60) {
+					$armaP = $this->database->getRandomWeapon("Primaria");
+					$this->$team[$i]["a"][$armaP];
+					$this->database->savePurchase($this->Codice, $this->round, $armaP, $this->$team[$i]["c"]);
+				}
+				if (rand() % 100 < 40) {
+					$armaS = $this->database->getRandomWeapon("Secondaria");
+					$this->$team[$i]["a"][$armaS];
+					$this->database->savePurchase($this->Codice, $this->round, $armaS, $this->$team[$i]["c"]);
+				}
+			}
+		}
+
+		$durata = rand(60, 180);
+		$attackers = $this->role == "Attacco" ? "Alpha" : "Beta";
+		$defenders = $this->role == "Difesa"  ? "Alpha" : "Beta";
+
+		$outcome = $this->generateRandomKills($this->$attackers, $this->$defenders, $durata);
+		if ($outcome === Game::A_TEAM_KILL) {
+			$squadra = $defenders;
+			$ruolo = "Difesa";
+		} else if ($outcome === Game::D_TEAM_KILL) {
+			$squadra = $attackers;
+			$ruolo = "Attacco";
+		} else { # no team kill, try to plant spike
+			$sito = $this->sites[rand() % count($this->sites)];
+			$giocatoreA = $outcome[0][rand() % count($outcome[0])]["c"]; # attackers
+			$giocatoreD = $outcome[1][rand() % count($outcome[1])]["c"]; # defenders
+			if (rand() % 100 > 50) { # spike planted
+				$this->database->saveAction($this->Codice, $this->round, "Innesco", $giocatoreA, $sito);
+				if (rand() % 100 > 50) # successful defuse
+					$this->database->saveAction($this->Codice, $this->round, "Disinnesco", $giocatoreD, $sito);
+			}
+		}
+
+		# save round
+		$this->database->saveRound($this->Codice, $this->round, gmdate("H:i:s", $durata), $squadra, $ruolo);
+		$this->round = $this->round + 1;
+	}
+
+	function generateRandomKills($attackers, $defenders, $duration) {
+		$teams = [ 0 => $attackers, 1 => $defenders ]; # copy and remap
+		$kills = rand(1, 10); # number of kills in this round
+		
+		# until players exist or enough kills are generated
+		for ($i = 0; count($teams[0]) && count($teams[1]) && $i < $kills; $i++) {
+			$c = rand() % 100 > 50; # randomly choose offender side
+			$s = !$c;
+			
+			$gc = $teams[$c][array_rand($teams[$c], 1)]; # offender player id
+			$gs = $teams[$s][$key = array_rand($teams[$s], 1)]; # killed player id
+			unset($teams[$s][$key]); # remove killed player
+				
+			$arma = $gc["a"][rand() % 2]; # select random weapon
+			# random uniform distribution
+			$time = 10 + intval($duration / ($kills * 1.5)) * (1 + $i) + ((rand() % 100 > 50) ? rand(1, 10) : rand(-10, -1));
+			if ($time > $duration) $time = $duration;
+			$this->database->saveKill($this->Codice, $this->round, $gs["c"], $gc["c"], gmdate("H:i:s", $time), $arma);
+		}
+		
+		if (!count($teams[0])) return Game::A_TEAM_KILL;
+		if (!count($teams[1])) return Game::D_TEAM_KILL;
+		return $teams;
 	}
 }
 
